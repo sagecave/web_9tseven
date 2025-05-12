7c734443eeca2b8535bda0ea5ba6ec627913e1c1
”sagecave” <Andreassage@gmail.com>
2025-05-12 13:33:43 +0200

    styling er done af products kort

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,11 +1,11 @@
 export default function ProductCards({ title, productImage, category, price }) {
   return (
-    <div className="  shadow-sm">
+    <div className="  hover:shadow-md">
       <img src={productImage} alt={title} className="w-full h-40 object-cover" />
       <div className="pt-4 pr-3 pl-3 pb-4">
-        <p className="text-HeaderSizeSmall font-medium text-main_black">{title}</p>
-        <p className="text-ParagraphSize text-main_black">{category}</p>
-        <p className="text-ParagraphSize text-main_black">{price}</p>
+        <p className="text-HeaderSizeSmall font-bold text-main_black">{title}</p>
+        <p className="text-ParagraphSize text-neutral_black">{category}</p>
+        <p className="text-ParagraphSize text-neutral_black">{price}</p>
       </div>
     </div>
   );
