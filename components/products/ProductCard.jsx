083a0d21a5d7b011b85132c586ea0ce94387649c
”sagecave” <Andreassage@gmail.com>
2025-05-12 12:31:34 +0200

    wait

--- components/products/ProductCard.jsx
+++ components/products/ProductCard.jsx
@@ -1,7 +1,9 @@
+"use client";
+import Image from "next/image";
 export default function ProductCards({ title, productImage }) {
   return (
     <div className="border border-primary-300 w-full rounded overflow-hidden shadow-sm">
-      <img src={productImage} alt={title} className="w-full h-40 object-cover" />
+      <Image src={productImage} alt={title} width={20} height={20} className="w-full h-40 object-cover" />
       <div className="bg-primary-100 h-24 flex items-center justify-center px-2">
         <p className="text-sm text-center">{title}</p>
       </div>
