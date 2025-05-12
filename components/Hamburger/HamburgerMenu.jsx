78eb5afa0159af4ea18b1a212e44e550c7254302
”sagecave” <Andreassage@gmail.com>
2025-05-12 12:06:45 +0200

    BurgerMenu work and almost done with styling

--- components/Hamburger/HamburgerMenu.jsx
+++ components/Hamburger/HamburgerMenu.jsx
@@ -1,7 +1,11 @@
 "use client";
 import Link from "next/link";
+import Image from "next/image";
 import { useState } from "react";
 import { motion, AnimatePresence } from "motion/react";
+import basketIcon from "../../public/assets/icons/basket.svg";
+import loop from "../../public/assets/icons/loop.svg";
+import personCircle from "../../public/assets/icons/personCircle.svg";
 
 const HamburgerMenu = () => {
   const [isOpen, setIsOpen] = useState(false);
@@ -11,63 +15,76 @@ const HamburgerMenu = () => {
   };
   return (
     <>
-      <div className="flex justify-between absolute w-full p-6 z-20 md:hidden">
+      <div className="flex justify-between absolute w-full  p-6 z-20 md:hidden">
         <button type="button" aria-expanded={isOpen} aria-label="Open Menu" onClick={handleMenuClick} className=" cursor-pointer">
           <div className="flex flex-col gap-1">
-            <span className=" w-6 h-0.5 bg-amber-400"></span>
-            <span className=" w-6 h-0.5 bg-amber-400"></span>
-            <span className=" w-6 h-0.5 bg-amber-400"></span>
+            <span className=" w-6 h-0.5 bg-main_white"></span>
+            <span className=" w-6 h-0.5 bg-main_white"></span>
+            <span className=" w-6 h-0.5 bg-main_white"></span>
           </div>
         </button>
         {isOpen && (
           <div>
-            <p>LOGO</p>
+            <Image src="/assets/images/logoWhite.png" alt="Logo" width={50} height={50} />
           </div>
         )}
-        {isOpen && (
+        {isOpen && <div></div>}
+        {!isOpen && (
           <ul className="flex gap-3 ">
             <li>
-              <Link href="#">SVG</Link>
+              <Link href="#">
+                <Image src={loop} alt="Logo" width={20} height={20} />
+              </Link>
             </li>
             <li>
-              <Link href="#">SVG</Link>
+              <Link href="#">
+                <Image src={personCircle} alt="Logo" width={20} height={20} />
+              </Link>
             </li>
             <li>
-              <Link href="#">SVG</Link>
+              <Link href="#">
+                <Image src={basketIcon} alt="Logo" width={20} height={20} />
+              </Link>
             </li>
           </ul>
         )}
       </div>
-      <nav className="w-full ">
-        <div className={"hidden  md:flex md:justify-evenly"}>
+      <nav className="w-full p-10 md:pt-6 md:pb-6 md:pl-10 md:pr-10 bg-main_black ">
+        <div className={"hidden    md:flex md:justify-between"}>
           <div>
             <ul className="flex gap-3 ">
-              <li>
-                <Link href="#">Shop</Link>
+              <li className="hover:text-main_white hover:underline">
+                <Link href="/productPage">Shop</Link>
               </li>
-              <li>
+              <li className="hover:text-main_white hover:underline">
                 <Link href="#">Community</Link>
               </li>
-              <li>
+              <li className="hover:text-main_white hover:underline">
                 <Link href="#">About</Link>
               </li>
             </ul>
           </div>
           <div>
-            <Link href="#">
-              <p>LOGO</p>
+            <Link href="/">
+              <Image src="/assets/images/logoWhite.png" alt="Logo" width={50} height={50} />
             </Link>
           </div>
           <div className={`${isOpen ? "hidden" : "block"}`}>
             <ul className="flex gap-3 ">
               <li>
-                <Link href="#">SVG</Link>
+                <Link href="#">
+                  <Image src={loop} alt="Logo" width={20} height={20} />
+                </Link>
               </li>
               <li>
-                <Link href="#">SVG</Link>
+                <Link href="#">
+                  <Image src={personCircle} alt="Logo" width={20} height={20} />
+                </Link>
               </li>
               <li>
-                <Link href="#">SVG</Link>
+                <Link href="#">
+                  <Image src={basketIcon} alt="Logo" width={20} height={20} />
+                </Link>
               </li>
             </ul>
           </div>
@@ -75,17 +92,17 @@ const HamburgerMenu = () => {
       </nav>
       <AnimatePresence>
         {isOpen && (
-          <motion.div className="w-full h-1/2 bg-blue-400 z-12 absolute" key="modal" initial={{ y: -300, opacity: 1 }} animate={{ y: 0, opacity: 1 }} exit={{ y: -300, opacity: 0 }} transition={{ ease: "easeOut", duration: 0.5 }}>
-            <div className=" mt-10 p-6">
+          <motion.div className="w-full h-1/2 bg-main_black z-12 absolute mt-[-1px] " key="modal" initial={{ y: -300, opacity: 1 }} animate={{ y: 0, opacity: 1 }} exit={{ y: -300, opacity: 0 }} transition={{ ease: "easeOut", duration: 0.5 }}>
+            <div className=" p-6">
               <div>
-                <ul className={`${isOpen ? "flex flex-col z-11" : "flex gap-3"}`}>
-                  <li>
+                <ul className={`${isOpen ? "flex flex-col z-11 text-HeaderSizeBig text-alternativ_white " : "flex gap-3"}`}>
+                  <li className="hover:text-main_white hover:underline">
                     <Link href="#">Shop</Link>
                   </li>
-                  <li>
+                  <li className="hover:text-main_white hover:underline">
                     <Link href="#">Community</Link>
                   </li>
-                  <li>
+                  <li className="hover:text-main_white hover:underline">
                     <Link href="#">About</Link>
                   </li>
                 </ul>
--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <Link href="/productPage" className=" text-amber-600">
-        Go to products
-      </Link>
       <Link href="/productPage/1" className=" text-amber-400">
         Go to testPage
       </Link>
