4e14c2afb28d834b71c38644a710870756f113d0
”sagecave” <Andreassage@gmail.com>
2025-05-13 19:42:15 +0200

    virker ikke

--- api/products.js
+++ api/products.js
@@ -1,17 +1,17 @@
+const url = "https://rqumbnvfrmsowdaxrvkm.supabase.co/rest/v1/9TSEVEN";
+const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdW1ibnZmcm1zb3dkYXhydmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjIzOTAsImV4cCI6MjA2MjYzODM5MH0.JpvYefKey3yiK285pXmaVs-tAR15E9OhoBp3bA22pKY";
+const options = {
+  method: "GET",
+  headers: {
+    apikey: API_KEY,
+    "Content-Type": "application/json",
+    // Authorization: "Bearer " + API_KEY,
+  },
+};
 export const fetchProducts = async () => {
   // const url = "https://dummyjson.com/products";
-  const url = "https://rqumbnvfrmsowdaxrvkm.supabase.co/rest/v1/9TSEVEN";
-  const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdW1ibnZmcm1zb3dkYXhydmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjIzOTAsImV4cCI6MjA2MjYzODM5MH0.JpvYefKey3yiK285pXmaVs-tAR15E9OhoBp3bA22pKY";
-  // const URL_full = url + "?apikey=" + API_KEY;
 
-  const options = {
-    method: "GET",
-    headers: {
-      apikey: API_KEY,
-      "Content-Type": "application/json",
-      // Authorization: "Bearer " + API_KEY,
-    },
-  };
+  // const URL_full = url + "?apikey=" + API_KEY;
 
   const response = await fetch(url, options);
   if (!response.ok) {
@@ -26,3 +26,16 @@ export const fetchProducts = async () => {
 //   const params = await segmentData.params
 //   const slug = params.slug
 // }
+
+export async function getProductSlug(slug, options) {
+  console.log("slug", slug);
+  let urlSingle = await fetch(`https://rqumbnvfrmsowdaxrvkm.supabase.co/rest/v1/9TSEVEN?slug_name=eq.${slug}`);
+  const response = await fetch(urlSingle, options);
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  let data = await response.json();
+  console.log(data);
+  return data;
+}
--- src/app/productPage/[slug]/page.js
+++ src/app/productPage/[slug]/page.js
@@ -1,7 +1,37 @@
-export default function SingleProductPage() {
+"use client";
+
+import { useQuery } from "@tanstack/react-query";
+import { getProductSlug } from "../../../../api/products";
+
+export default function ProductDetails({ slug }) {
+  const { data, isLoading, error } = useQuery({
+    queryKey: ["productSlug", slug],
+    queryFn: () => getProductSlug(slug), // Pass the slug to the function
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p className="text-red-500">Error: {error.message}</p>;
+  }
+
   return (
     <div>
-      <h1 className=" text-amber-300">ME LOL</h1>
+      <h1>{data[0]?.title}</h1>
+      <p>{data[0]?.description}</p>
+      <p>Price: ${data[0]?.price}</p>
     </div>
   );
 }
+
+// export default function SingleProductPage() {
+//   return (
+//     <div>
+//       <h1 className=" text-amber-300">ME LOL</h1>
+//     </div>
+//   );
+// }
+
+// getProductSlug
