808de668b9f2eac88343ebdbaca3e358f30ac042
”sagecave” <Andreassage@gmail.com>
2025-05-20 10:43:14 +0200

    stripe er rykket over til payment page

---
+++ components/StripeComponent/Payment.jsx
@@ -0,0 +1,39 @@
+"use client";
+
+import CheckoutPage from "../stripe/CheckoutPage";
+import { convertToSubcurrency } from "../../lib/convertToSubcurrency";
+import { Elements } from "@stripe/react-stripe-js";
+import { loadStripe } from "@stripe/stripe-js";
+
+if (process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY === undefined) {
+  throw new Error("Missing Stripe publishable key");
+}
+
+const stripePromise = loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLIC_KEY);
+const payment = () => {
+  const amount = 1000;
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className=" bg-pink-500 w-1/2 p-6 flex flex-col justify-center items-center rounded-lg shadow-lg">
+        <h1 className="text-HeaderSizeSmall text-main_black pb-[20]">Checkout</h1>
+        <p className="text-HeaderSizeSmall text-main_black pb-[20]">
+          Has requested <span>{amount}</span>
+        </p>
+        <div className="flex justify-center items-center">
+          <Elements
+            stripe={stripePromise}
+            options={{
+              mode: "payment",
+              amount: convertToSubcurrency(amount),
+              currency: "usd",
+            }}
+          >
+            <CheckoutPage amount={amount} />
+          </Elements>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default payment;
---
+++ src/app/paymentPage/page.js
@@ -0,0 +1,9 @@
+import Payment from "../../../components/StripeComponent/Payment";
+
+export default function Home() {
+  return (
+    <div>
+      <Payment></Payment>
+    </div>
+  );
+}
