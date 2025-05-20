5969d369885fdab7e848b5f50aff86d879c0275a
”sagecave” <Andreassage@gmail.com>
2025-05-20 13:47:14 +0200

    total price virker

--- components/basket/BasketModal.jsx
+++ components/basket/BasketModal.jsx
@@ -1,17 +1,19 @@
+"use client";
 import { motion, AnimatePresence } from "motion/react";
-import Image from "next/image";
-import Link from "next/link";
-import basketIcon from "../../public/assets/icons/basket.svg";
-import loop from "../../public/assets/icons/loop.svg";
-import personCircle from "../../public/assets/icons/personCircle.svg";
 import BasketProductCard from "./BasketProductCard";
-import { create } from "zustand";
 import { useBasketStore } from "../../globalHooks/basketProduct";
 import { useStore } from "../../globalHooks/basketHooks";
+import { useState, useEffect, use } from "react";
 
 const BasketModal = (isBasketOpen, setBasketOpen) => {
   const { BasketSatete } = useBasketStore();
-  const quantatityAndPriceCalculater = useStore((state) => state.quantatityAndPriceCalculater);
+  const [productPriceArray, setProductPriceArray] = useState([]);
+  const productQuantities = useStore((state) => state.productQuantities);
+
+  const totalPrices = BasketSatete.reduce((acc, product) => {
+    const quantity = productQuantities[product.id] || 0;
+    return acc + product.price * quantity;
+  }, 0);
   console.log("BasketSatete modal", BasketSatete);
   return (
     <div>
@@ -27,14 +29,32 @@ const BasketModal = (isBasketOpen, setBasketOpen) => {
           >
             <h2 className="text-main_white">Your items</h2>
             <div className="snap-y snap-mandatory overflow-y-scroll h-full ">
-              <div>{BasketSatete.map((product) => (console.log("product", product), (<BasketProductCard key={product.id} id={product.id} title={product.title} productImages={product.first_image} item={product.item} price={product.price} />)))}</div>
+              <div>
+                {BasketSatete.map(
+                  (product) => (
+                    console.log("product", product),
+                    (
+                      <BasketProductCard
+                        key={product.id}
+                        productPriceArray={productPriceArray}
+                        setProductPriceArray={setProductPriceArray}
+                        id={product.id}
+                        title={product.title}
+                        productImages={product.first_image}
+                        item={product.item}
+                        price={product.price}
+                      />
+                    )
+                  )
+                )}
+              </div>
             </div>
             <div>
               <div className="flex flex-row justify-between items-center">
                 <p className="text-main_white">Total</p>
-                <p className="text-main_white">100,-</p>
+                <p className="text-main_white">{totalPrices},-</p>
               </div>
-              <button className="p-4 bg-main_white hover:bg-alternativ_black hover:text-main_white text-main_black">checkout</button>
+              <button className="p-4 w-full bg-main_white hover:bg-alternativ_black hover:text-main_white text-main_black">checkout</button>
             </div>
           </motion.div>
         )}
--- components/basket/BasketProductCard.jsx
+++ components/basket/BasketProductCard.jsx
@@ -1,12 +1,12 @@
 import { useStore } from "../../globalHooks/basketHooks";
 import { useBasketStore } from "../../globalHooks/basketProduct";
 // removeOneProduct
-const BasketProductCard = ({ title, id, productImages, item, price }) => {
+const BasketProductCard = ({ title, id, productImages, item, price, setProductPriceArray, productPriceArray }) => {
   const increaseProductNumber = useStore((state) => state.increaseProductNumber);
   const decreaseProductNumber = useStore((state) => state.decreaseProductNumber);
-  const productQuantities = useStore((state) => state.productQuantities);
   const removeProductQuantities = useStore((state) => state.removeProductQuantities);
   const remove = useBasketStore((state) => state.remove);
+  const productQuantities = useStore((state) => state.productQuantities);
   const quantity = productQuantities[id] || 1;
 
   function handleDecrease(id) {
@@ -16,6 +16,11 @@ const BasketProductCard = ({ title, id, productImages, item, price }) => {
       remove(id);
       removeProductQuantities(id);
     }
+  }
+  function handleIncrease(id) {
+    increaseProductNumber(id);
+
+    console.log("productPriceArray", productPriceArray);
   }
 
   return (
@@ -29,7 +34,7 @@ const BasketProductCard = ({ title, id, productImages, item, price }) => {
         </div>
       </div>
       <div className="flex flex-row gap-2 items-center">
-        <button className="p-1 w-6 h-6 text-center flex items-center justify-center rounded-full bg-main_white text-main_black hover:bg-alternativ_black hover:text-main_white" onClick={() => increaseProductNumber(id)}>
+        <button className="p-1 w-6 h-6 text-center flex items-center justify-center rounded-full bg-main_white text-main_black hover:bg-alternativ_black hover:text-main_white" onClick={() => handleIncrease(id)}>
           +
         </button>
         <p>{quantity}</p>
