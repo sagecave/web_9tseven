7b60408b57796411425e1ec2d95c866e4ce7ed19
”sagecave” <Andreassage@gmail.com>
2025-05-23 12:01:09 +0200

    single page almost done

--- components/frontPageProductsShow/productContainer.jsx
+++ components/frontPageProductsShow/productContainer.jsx
@@ -24,24 +24,27 @@ const ProductContainer = () => {
     );
   }
   return (
-    <section className="grid grid-cols-2 grid-rows-3 h-[100vh]">
-      <div className="col-start-1 row-start-1 col-end-3 row-end-3">
-        <div className="">
-          <div>
-            {productData.map((item) => (
-              <ProductCard key={item.id} productImage={item.first_image} title={item.title} category={item.category} price={item.price} slugName={item.slug_name} />
-            ))}
+    <section className="grid lg:grid-cols-2 lg:grid-rows-3 lg:h-[100vh] ">
+      <div className="lg:col-start-1 lg:row-start-1 lg:col-end-3 lg:row-end-3">
+        <div>
+          <div className="grid grid-cols-2 gap-4 lg:pr-4">
+            {productData
+              .slice(2, 3)
+              .concat(productData.slice(0, 1))
+              .map((item) => (
+                <ProductCard key={item.id} productImage={item.first_image} title={item.title} category={item.category} price={item.price} slugName={item.slug_name} />
+              ))}
           </div>
         </div>
       </div>
-      <div className="col-start-1 row-start-3 col-end-3">
+      <div className="lg:col-start-1 lg:row-start-3 lg:col-end-3">
         <h2 className=" text-main_black">GRATITUDE</h2>
-        <p className="w-[20rem] text-alternativ_black">
+        <p className="md:w-[20rem] text-alternativ_black">
           Gratitude means noticing and appreciating the good things in life. It can boost your mood and help you feel more connected. With Social Run, it’s easier to feel grateful — whether it's for a shared smile, a team effort, or just showing up
           together. Running with others brings small moments that mean a lot.
         </p>
       </div>
-      <div className="col-start-3 row-start-1 row-end-4">
+      <div className="lg:col-start-3 row-start-1 lg:row-end-4 row-span-1">
         <Image src="/assets/images/firstProductShow.jpg" alt="hero" width={500} height={500} />
       </div>
     </section>
