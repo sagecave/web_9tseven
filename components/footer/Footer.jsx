6b2a17ad02524f516b2460d665c645f0aa806fb6
”sagecave” <Andreassage@gmail.com>
2025-05-23 14:29:42 +0200

    sjjjjj

--- components/footer/Footer.jsx
+++ components/footer/Footer.jsx
@@ -2,15 +2,15 @@ import Link from "next/link";
 import Image from "next/image";
 const Footer = () => {
   return (
-    <div className="bg-main_black w-full h-auto pt-8 pb-8 pr-14 pl-14 ">
-      <div className="flex justify-center mb-5 ">
+    <div className="bg-main_black w-full h-auto pt-8 pb-8 md:pr-14 md:pl-14 pr-2 pl-2">
+      <div className="flex justify-center mb-5 md:hidden">
         <Image src="/assets/images/logoWhite.png" alt="Logo" width={50} height={50} />
       </div>
-      <div className="flex flex-row justify-between">
+      <div className="flex flex-row md:justify-between">
         <div className="flex flex-col gap-5">
           <h2 className=" text-HeaderSizeSmall">JOIN THE 9TSEVEN NEWSLETTER</h2>
           <form>
-            <input className=" border-2 border-main_white w-[20rem] h-[3rem] p-2" type="text" placeholder="Email address" name="mail" required></input>
+            <input className=" border-2 border-main_white w-auto md:w-[20rem] h-[3rem] p-2" type="text" placeholder="Email address" name="mail" required></input>
             <input className="border-2 border-main_white border-l-[0] w-[5rem] h-[3rem] p-2 hover:bg-alternativ_black " type="submit" />
 
             <div>
--- src/app/page.js
+++ src/app/page.js
@@ -5,12 +5,10 @@ import ProductLayout from "../../layouts/ProductLayout";
 export default function Home() {
   return (
     <>
-      <section className="h-[100vh] col-start-1 col-end-4 grid overflow-hidden">
-        <Image src="/assets/images/frontPageFirstPic.jpg" alt="hero" width={1000} height={1000} className="w-full h-full  overflow-hidden  object-cover " />
-
-        <h1 className="text-alternativ_black">Front page</h1>
+      <section className="h-[100vh] col-start-1 col-end-4 grid overflow-hidden  relative">
+        <Image src="/assets/images/frontPageFirstPic.jpg" alt="hero" fill className="md:w-full h-full  overflow-hidden  object-cover " />
       </section>
-      <section className="grid col-start-2 col-end-3 mt-10">
+      <section className="grid lg:col-start-2 lg:col-end-3 col-start-1 col-end-4 mt-10">
         <ProductLayout>
           <ProductContainer />
         </ProductLayout>
