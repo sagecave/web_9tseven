76a0a091c5dde4125e7b0b2d013857fc200f9acd
”sagecave” <Andreassage@gmail.com>
2025-05-23 15:16:43 +0200

    mangler bare lidt justeringer

--- src/app/page.js
+++ src/app/page.js
@@ -13,6 +13,28 @@ export default function Home() {
           <ProductContainer />
         </ProductLayout>
       </section>
+      <section className="h-[100vh] col-start-1 col-end-4 grid overflow-hidden  relative">
+        <div className="flex">
+          <Image src="/assets/images/frontPageSecoundPicColor.jpg" alt="hero" width={1000} height={1000} className="md:w-full h-full  overflow-hidden  object-cover hover:saturate-50 " />
+          <Image src="/assets/images/frontPageSecoundPic2.jpg" alt="hero" width={1000} height={1000} className="md:w-full h-full  overflow-hidden  object-cover  saturate-0 hover:saturate-100" />
+        </div>
+      </section>
+      <section className="grid lg:col-start-2 lg:col-end-3 col-start-1 col-end-4 mt-10">
+        <ProductLayout>
+          <ProductContainer />
+        </ProductLayout>
+      </section>
+      <section className="h-[100vh] col-start-1 col-end-4 grid overflow-hidden  relative">
+        <Image src="/assets/images/frontPageThridPic.jpg" alt="hero" fill className="md:w-full h-full  overflow-hidden  object-cover " />
+      </section>
+      <section className="grid lg:col-start-2 lg:col-end-3 col-start-1 col-end-4 mt-10">
+        <ProductLayout>
+          <ProductContainer />
+        </ProductLayout>
+      </section>
+      <section className="h-[100vh] col-start-1 col-end-4 grid overflow-hidden  relative">
+        <Image src="/assets/images/frontPageFourPic.jpg" alt="hero" fill className="md:w-full h-full  overflow-hidden  object-cover " />
+      </section>
     </>
   );
 }
