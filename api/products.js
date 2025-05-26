eafa1460143bf6b5ee45be5a5368e638071e63e8
”sagecave” <Andreassage@gmail.com>
2025-05-26 13:21:35 +0200

    clean code

--- api/products.js
+++ api/products.js
@@ -1,15 +1,12 @@
 export const fetchProducts = async () => {
-  // const url = "https://dummyjson.com/products";
   const url = "https://rqumbnvfrmsowdaxrvkm.supabase.co/rest/v1/9TSEVEN";
   const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdW1ibnZmcm1zb3dkYXhydmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjIzOTAsImV4cCI6MjA2MjYzODM5MH0.JpvYefKey3yiK285pXmaVs-tAR15E9OhoBp3bA22pKY";
-  // const URL_full = url + "?apikey=" + API_KEY;
 
   const options = {
     method: "GET",
     headers: {
       apikey: API_KEY,
       "Content-Type": "application/json",
-      // Authorization: "Bearer " + API_KEY,
     },
   };
 
@@ -21,8 +18,3 @@ export const fetchProducts = async () => {
   console.log(json);
   return json;
 };
-
-// export async function singleProductFetch(request, segmentData) {
-//   const params = await segmentData.params
-//   const slug = params.slug
-// }
