a3a517f82006ea45a23ea8d8e27cd430614bb71b
”sagecave” <Andreassage@gmail.com>
2025-05-27 12:48:25 +0200

    env

--- api/products.js
+++ api/products.js
@@ -1,8 +1,6 @@
 export const fetchProducts = async () => {
   const url = process.env.NEXT_PUBLIC_DATA_SUPABASE_URL;
   const API_KEY = process.env.NEXT_PUBLIC_DATA_SUPABASE_ANON_KEY;
-  // const url = "https://rqumbnvfrmsowdaxrvkm.supabase.co/rest/v1/9TSEVEN";
-  // const API_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJxdW1ibnZmcm1zb3dkYXhydmttIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDcwNjIzOTAsImV4cCI6MjA2MjYzODM5MH0.JpvYefKey3yiK285pXmaVs-tAR15E9OhoBp3bA22pKY";
 
   const options = {
     method: "GET",
