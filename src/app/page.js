5bfa1f2af3e569e33080da8ffc55bc4430a51378
”sagecave” <Andreassage@gmail.com>
2025-05-27 15:40:19 +0200

    producter matcher

--- src/app/page.js
+++ src/app/page.js
@@ -38,10 +38,10 @@ export default function Home() {
       <section className="grid md:col-start-2 md:col-end-3 col-start-1 col-end-4 md:mt-30 md:mb-15">
         <ProductLayout>
           <ProductContainer
-            slice1="2"
-            slice2="3"
-            slice3="0"
-            slice4="1"
+            slice1="5"
+            slice2="6"
+            slice3="7"
+            slice4="8"
             images="sittingGlasses"
             header="GRATITUDE"
             paragraph="Gratitude means noticing and appreciating the good things in life. It can boost your mood and help you feel more connected. With Social Run, it’s easier to feel grateful — whether it's for a shared smile, a team effort, or just showing up together. Running with others brings small moments that mean a lot."
@@ -92,8 +92,8 @@ With Social Run, everyone is invited — no matter your background, pace, or exp
           <ProductContainer
             slice1="8"
             slice2="9"
-            slice3="1"
-            slice4="2"
+            slice3="6"
+            slice4="7"
             images="sammySideways"
             header="WELCOME ALL FEELINGS"
             paragraph="Welcome all feelings means it's okay to feel happy, tired, nervous, or excited — all emotions are valid. You don't have to hide how you feel.
